5fb567be0ed6b73c4129812520fbedf0e117bdb3
zhupf <530675553@qq.com>
2019-02-28 14:17:12 +0800

    -

--- api/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("MYSQLNAME", "PizzaExpress");
-    define("BASE_URL", '');
-    define("PROJECT_NAME","PizzaExpress");
-    define("DBUSER","root");
-    define("DBPASSWORD","");
-?>
