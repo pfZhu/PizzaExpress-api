a9bbcb1de0bd46da3f64982610a500f80f2ca5fb
zhupf <530675553@qq.com>
2019-04-12 09:55:33 +0800

    增加工厂管理

---
+++ Demo/Api/Factory.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * 工厂管理类
+ */
+
+class Api_Factory extends PhalApi_Api {
+
+    public function getRules() {
+        return array(
+            'getFactoryList' => array(),
+            'addFactory' => array(
+                'name' => array('name' => 'name', 'type' => 'string', 'require' => true, 'desc' => '名字'),
+                'lat' => array('name' => 'lat', 'type' => 'float', 'require' => true, 'desc' => '纬度'),
+                'lng' => array('name' => 'lng', 'type' => 'float', 'require' => true, 'desc' => '经度'),
+                'phone' => array('name' => 'phone', 'type' => 'string', 'require' => true, 'desc' => '电话'),
+                'address' => array('name' => 'address', 'type' => 'string', 'require' => true, 'desc' => '地址'),
+            ),
+            'deleteFactory' => array(
+                'factoryId' => array('name' => 'factoryId', 'type' => 'int', 'require' => true, 'desc' => '工厂id'),
+
+            ),
+            'updateFactory' => array(
+                'factoryId' => array('name' => 'factoryId', 'type' => 'int', 'require' => true, 'desc' => '工厂id'),
+                'name' => array('name' => 'name', 'type' => 'string', 'require' => true, 'desc' => '名字'),
+                'lat' => array('name' => 'lat', 'type' => 'float', 'require' => true, 'desc' => '纬度'),
+                'lng' => array('name' => 'lng', 'type' => 'float', 'require' => true, 'desc' => '经度'),
+                'phone' => array('name' => 'phone', 'type' => 'string', 'require' => true, 'desc' => '电话'),
+                'address' => array('name' => 'address', 'type' => 'string', 'require' => true, 'desc' => '地址'),
+            ),
+        );
+    }
+    /**
+     * 获取工厂列表
+     * @desc 用于获取工厂列表
+     */
+    public function getFactoryList() {
+        $domain = new Domain_Storage();
+        $rs = $domain->getFactoryList();
+        return $rs;
+    }
+
+    /**
+     * 添加工场
+     * @desc
+     */
+    public function addFactory() {
+        $model=new Model_Factory();
+        return $model->insertFactory(array("name"=>$this->name,"lat"=>$this->lat,"lng"=>$this->lng,"phone"=>$this->phone,"address"=>$this->address));
+    }
+
+    /**
+     * 获取工厂列表
+     * @desc 用于获取工厂列表
+     */
+    public function deleteFactory() {
+        $model=new Model_Factory();
+        return $model->deleteFactoryById($this->factoryId);
+    }
+
+    /**
+     * 获取工厂列表
+     * @desc 用于获取工厂列表
+     */
+    public function updateFactory() {
+        $model=new Model_Factory();
+        $data=array("name"=>$this->name,"lat"=>$this->lat,"lng"=>$this->lng,"phone"=>$this->phone,"address"=>$this->address);
+        return $model->updateFactory($this->factoryId,$data);
+    }
+
+
+
+}
